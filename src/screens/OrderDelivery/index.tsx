3765555e85cf13e25bba29ac03973d0beefbb58c
Felipe Simmi <simmifelipe1992@gmail.com>
2020-12-21 22:24:31 -0300

    Commit inicial

---
+++ src/screens/OrderDelivery/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const OrderDelivery: React.FC = () => {
+  return (
+    <View>
+      <Text>OrderDelivery</Text>
+    </View>
+  );
+};
+
+export default OrderDelivery;
